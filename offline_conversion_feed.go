1a91c55504e8723dd84f24fa027ce5c7cdd0bf4b
paulaan <weburnittv@gmail.com>
2019-03-12 10:17:51 +0700

    [offline-conversion-upload] Add Mutate for Offline conversion upload

--- offline_conversion_feed.go
+++ offline_conversion_feed.go
@@ -1,9 +1,63 @@
 package gads
 
+import "encoding/xml"
+
 type OfflineConversionService struct {
 	Auth
 }
 
+type OfflineConversionFeed struct {
+	GoogleClickId             string  `xml:"googleClickId"`
+	ConversionName            string  `xml:"conversionName"`
+	ConversionTime            string  `xml:"conversionTime"`
+	ConversionCurrencyCode    string  `xml:"conversionCurrencyCode"`
+	ExternalAttributionModel  string  `xml:"externalAttributionModel"`
+	ConversionValue           float32 `xml:"conversionValue"`
+	ExternalAttributionCredit float32 `xml:"externalAttributionCredit"`
+}
+
+type OfflineConversionOperations map[string][]OfflineConversionFeed
+
 func NewOfflineConversionService(auth *Auth) *OfflineConversionService {
 	return &OfflineConversionService{Auth: *auth}
+}
+
+func (o *OfflineConversionService) Mutate(conversionOperations OfflineConversionOperations) (conversion []OfflineConversionFeed, error error) {
+	type conversionOperation struct {
+		Action     string                `xml:"operator"`
+		Conversion OfflineConversionFeed `xml:"operand"`
+	}
+	var ops []conversionOperation
+
+	for action, conversion := range conversionOperations {
+		for _, con := range conversion {
+			ops = append(ops,
+				conversionOperation{
+					Action:     action,
+					Conversion: con,
+				},
+			)
+		}
+	}
+	mutation := struct {
+		XMLName xml.Name
+		Ops     []conversionOperation `xml:"operations"`
+	}{
+		XMLName: xml.Name{
+			Space: baseUrl,
+			Local: "mutate",
+		},
+		Ops: ops}
+	respBody, err := o.Auth.request(campaignServiceUrl, "mutate", mutation)
+	if err != nil {
+		return conversion, err
+	}
+	mutateResp := struct {
+		Conversions []OfflineConversionFeed `xml:"rval>value"`
+	}{}
+	err = xml.Unmarshal([]byte(respBody), &mutateResp)
+	if err != nil {
+		return conversion, err
+	}
+	return mutateResp.Conversions, nil
 }
