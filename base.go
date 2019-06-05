e1100e9b16f9e9ecae12502de697cb5b69cfc322
paulaan <weburnittv@gmail.com>
2019-06-05 12:11:53 +0700

    [master] Return faultstring

--- base.go
+++ base.go
@@ -203,13 +203,13 @@ func (a *Auth) request(serviceUrl ServiceUrl, action string, body interface{}) (
 		return respBody, err
 	}
 	if resp.StatusCode == 400 || resp.StatusCode == 401 || resp.StatusCode == 403 || resp.StatusCode == 405 || resp.StatusCode == 500 {
-		fault := Fault{}
+		fault := new(Fault)
 		fmt.Printf("unknown error ->\n%s\n", string(soapResp.Body.Response))
-		err = xml.Unmarshal(soapResp.Body.Response, &fault)
+		err = xml.Unmarshal(soapResp.Body.Response, fault)
 		if err != nil {
 			return respBody, err
 		}
-		return soapResp.Body.Response, &fault.Errors
+		return soapResp.Body.Response, fault
 	}
 	return soapResp.Body.Response, err
 }
